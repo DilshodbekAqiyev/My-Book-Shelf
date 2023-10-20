08ef3f9c540f3bea230483e141321577bf74110a
Dilshodbek <dilshodjonaqiyev@gmail.com>
2023-10-21 01:33:15 +0500

    added firebase and get books

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Main from './main.jsx'
-import './index.css'
-import { I18nextProvider } from 'react-i18next'
-import i18n from './i18n.js'
 
+import '../firebase'
+
+import i18n from './i18n.js'
+import { I18nextProvider } from 'react-i18next'
+
+import Main from './main.jsx'
+
+import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n} initialLanguage={'uz'}>
