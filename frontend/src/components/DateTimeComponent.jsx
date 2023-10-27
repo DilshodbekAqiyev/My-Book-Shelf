5cb5c166a8229da01f0ad413d551c6d5e199f740
Dilshodbek <dilshodjonaqiyev@gmail.com>
2023-10-27 20:34:44 +0500

    backend add

--- frontend/src/components/DateTimeComponent.jsx
+++ frontend/src/components/DateTimeComponent.jsx
@@ -1,10 +1,19 @@
 import dayjs from 'dayjs'
 import { WiTime9 } from 'react-icons/wi'
 import { FcCalendar } from 'react-icons/fc'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 function DateTimeComponent() {
-  const currentTime = dayjs().format('hh:mm A')
-  const currentDate = dayjs().format('D-MMM-YYYY')
+  const [currentTime, setCurrentTime] = useState('')
+  const [currentDate, setCurrentDate] = useState('')
+  // const currentTime = dayjs().format('hh:mm A')
+  // const currentDate = dayjs().format('D-MMM-YYYY')
+
+  useEffect(() => {
+    setCurrentTime(dayjs().format('hh:mm A'))
+    setCurrentDate(dayjs().format('D-MMM-YYYY'))
+  }, [])
 
   return (
     <div className="rounded-full bg-white shadow-md px-[30px] h-[40px] flex justify-between gap-5 items-center">
